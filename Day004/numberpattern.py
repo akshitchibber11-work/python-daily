ce0061e559eb30012e2a276f5c295a6336f18aeb
Akshit Chibber <akshitchibber11@gmail.com>
2026-01-31 02:25:06 +0530

    Day004: Star and Number Triangle pattern

---
+++ Day004/numberpattern.py
@@ -0,0 +1,6 @@
+#Number pattern in python using basic double for loops
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j, end="")
+    print()
---
+++ Day004/starpattern.py
@@ -0,0 +1,6 @@
+#Star pattern in python using basic double for loops
+
+for i in range(1,5):
+    for j in range(i):
+        print("*", end="")
+    print()
